6389fe3f553ad769fe1e373c3176cdc494a8ca2b
Cristian R <rionutc94@gmail.com>
2023-05-26 19:39:24 +0300

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -26,7 +26,7 @@ elif [[ "${IGNORE_TABLES}" ]]; then
  aws s3 --endpoint=https://$S3_URL cp $DB_BACKUP_PATH/$DB_NAME-$CURRENT_DATE-2.sql s3://${S3_BUCKET}/db/
 else
  echo "✅Creating backup for entire database"
- mysqldump -h "$DB_HOST" -u $DB_USER -p"$DB_PASS" $DB_NAME --verbose  | gzip > $DB_BACKUP_PATH/$DB_NAME-$CURRENT_DATE-90.sql
+ mysqldump -h "$DB_HOST" -u $DB_USER -p"$DB_PASS" $DB_NAME --verbose  | gzip > $DB_BACKUP_PATH/$DB_NAME-$CURRENT_DATE-90.sql.gz
  echo "🚧 Uploading mysql dump ($DB_NAME-$CURRENT_DATE.sql) to s3 ..."
  aws s3 --endpoint=https://$S3_URL cp $DB_BACKUP_PATH/$DB_NAME-$CURRENT_DATE-90.sql s3://${S3_BUCKET}/db/
 fi
